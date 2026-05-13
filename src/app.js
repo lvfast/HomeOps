d2466b6d251e9f34f73e5d0eece6a55bd9b3caa6
lvfast <vinhphatluu23@gmail.com>
2026-05-13 20:12:58 +0700

    refactor(api): move not found handler into middleware

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 const express = require("express");
+const notFoundHandler = require("./middleware/notFoundHandler");
 const statusRoutes = require("./routes/statusRoutes");
 
 const app = express();
 
 app.use(statusRoutes);
-
-app.use((req, res) => {
-  res.status(404).json({
-    error: "Not Found",
-    message: "Route not found",
-  });
-});
+app.use(notFoundHandler);
 
 module.exports = app;
