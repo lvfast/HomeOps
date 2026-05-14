2c960fdd84346e993bfea84a43c1df9212765175
lvfast <vinhphatluu23@gmail.com>
2026-05-14 08:22:04 +0700

    feat(api): parse JSON request bodies

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,4 +1,11 @@
 function errorHandler(err, req, res, next) {
+  if (err instanceof SyntaxError && err.status === 400 && "body" in err) {
+    return res.status(400).json({
+      error: "Bad Request",
+      message: "Invalid JSON request body",
+    });
+  }
+
   res.status(500).json({
     error: "Internal Server Error",
     message: "Something went wrong",
